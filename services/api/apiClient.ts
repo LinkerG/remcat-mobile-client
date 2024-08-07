18f94ef3a2ce9f65cf63d55300d5d8dce907bb3c
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2024-08-07 13:32:05 +0200

    [Feature] Gestion de secretos

--- services/api/apiClient.ts
+++ services/api/apiClient.ts
@@ -1,11 +1,18 @@
 import axios from "axios"
 import { setupCache } from 'axios-cache-adapter';
+import Constants from "expo-constants";
 
 const cache = setupCache({
     maxAge: 15 * 60 * 1000 // 15 minutos
 });
 
+const API_KEY = Constants.expoConfig?.extra?.api_key;
+const API_URL = Constants.expoConfig?.extra?.api_url;
+
 export const apiClient = axios.create({
-    baseURL: "http://192.168.1.36:1097/api/v1",
+    baseURL: API_URL,
     adapter: cache.adapter,
+    headers: {
+        api_key: API_KEY
+    },
 })
