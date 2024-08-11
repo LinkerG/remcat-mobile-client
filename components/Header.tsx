7fa2c72f38bc19adaf5561a884180918769c9916
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2024-08-11 14:27:19 +0200

    [Feature] Modal para cambiar de año

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { View, Text, Pressable } from "react-native";
+import { useDispatch } from 'react-redux';
+import { openModal } from '../store/reducers/selectYearModal';
 
 export default function Header() {
+    const dispatch = useDispatch();
+
     return (
         <View className="w-100 bg-blue-400 ">
+
             <View className="flex h-16 flex-row justify-between items-center">
                 <Text className="ml-5">
                     RemCat
                 </Text>
-                <Pressable className="mr-5">
+                <Pressable
+                    className="mr-5"
+                    onPress={() => dispatch(openModal())}
+                >
                     <Text>
-                        Boton
+                        Cambiar año
                     </Text>
                 </Pressable>
             </View>
---
+++ store/reducers/selectYearModal.ts
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+interface ModalState {
+    isOpen: boolean;
+}
+
+const initialState: ModalState = {
+    isOpen: false,
+};
+
+const modalSlice = createSlice({
+    name: 'modal',
+    initialState,
+    reducers: {
+        openModal: (state) => {
+            state.isOpen = true;
+        },
+        closeModal: (state) => {
+            state.isOpen = false;
+        },
+        toggleModal: (state) => {
+            state.isOpen = !state.isOpen;
+        },
+    },
+});
+
+export const { openModal, closeModal, toggleModal } = modalSlice.actions;
+
+export default modalSlice.reducer;
