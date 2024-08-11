04c56aff82ac113c8c9954ec179a914ebf1e5885
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2024-08-11 17:24:12 +0200

    Soporte para DNS y DNF en tiempo

--- services/types/result.ts
+++ services/types/result.ts
@@ -1,30 +1,32 @@
 export type Result = {
-    _id: string
-    competition_id: string
-    teamShortName: string
-    category: string
-    time: string
-    isFinal: boolean
-    isValid: boolean
+    _id: string;
+    competition_id: string;
+    teamShortName: string;
+    category: string;
+    time: string;
+    isFinal: boolean;
+    isValid: boolean;
 }
 
 function convertToMilliseconds(time: string): number {
+    if (time === "DNS" || time === "DNF") {
+        // Se devuele un numero demasiado grande para que la funcion no lo clasifique
+        return Number.MAX_SAFE_INTEGER;
+    }
+
     const [minutes, seconds, milliseconds] = time.split(':').map(Number);
     return (minutes * 60 * 1000) + (seconds * 1000) + milliseconds;
 }
 
 export function sortResults(results: Result[]): Result[] {
-    // Filtrar los resultados válidos y no válidos
-    const validResults = results.filter(result => result.isValid);
-    const invalidResults = results.filter(result => !result.isValid);
+    const validResults = results.filter(result => result.isValid && result.time !== "DNS" && result.time !== "DNF");
+    const dnsOrInvalidResults = results.filter(result => !result.isValid || result.time === "DNS" || result.time === "DNF");
 
-    // Ordenar los resultados válidos por tiempo
     validResults.sort((a, b) => {
         const timeA = convertToMilliseconds(a.time);
         const timeB = convertToMilliseconds(b.time);
         return timeA - timeB;
     });
 
-    // Combinar los resultados válidos ordenados con los no válidos al final
-    return [...validResults, ...invalidResults];
+    return [...validResults, ...dnsOrInvalidResults];
 }
