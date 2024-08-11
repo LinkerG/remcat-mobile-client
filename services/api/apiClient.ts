d1dbd596badc8e9147f631f7273c529583d0be01
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2024-08-11 12:52:20 +0200

    [Debug] logs para comprobar cache

--- services/api/apiClient.ts
+++ services/api/apiClient.ts
@@ -1,19 +1,32 @@
-import axios from "axios"
+import axios from "axios";
 import { setupCache } from 'axios-cache-adapter';
 import Constants from "expo-constants";
 
 const cache = setupCache({
-    maxAge: 15 * 60 * 1000 // 15 minutos
+    maxAge: 15 * 60 * 1000, // 15 minutes
+    debug: true, // Enable debugging to see cache hits/misses
+    exclude: {
+        query: false // Cache requests with query parameters
+    },
+    key: (req) => {
+        // Custom key generation, e.g., use the full URL + query params
+        return req.url + '?' + new URLSearchParams(req.params).toString();
+    }
 });
 
 const smallerCache = setupCache({
-    maxAge: 1 * 60 * 1000 // 15 minutos
+    maxAge: 1 * 60 * 1000, // 1 minute
+    debug: true,
+    exclude: {
+        query: false
+    }
 });
 
-// Se pone asigna de esta manera los valores ya que la web lee las variables de 
-// entorno de una manera diferente que Android/IOS
+// Environment variable handling
 const API_KEY = Constants.expoConfig?.extra?.api_key || process.env['API_KEY'];
 const API_URL = Constants.expoConfig?.extra?.api_url || process.env['API_URL'];
+
+console.log(API_URL);
 
 export const apiClient = axios.create({
     baseURL: API_URL,
