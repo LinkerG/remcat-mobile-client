a05d5428cd4683bbe829e2336aa1c27fe33dee29
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2024-08-17 01:20:46 +0200

    [Remove] Consola limpia

--- api/apiClient.ts
+++ api/apiClient.ts
@@ -1,26 +1,6 @@
 import axios from "axios";
-import { setupCache } from 'axios-cache-adapter';
 import Constants from "expo-constants";
 
-const cache = setupCache({
-    maxAge: 15 * 60 * 1000, // 15 minutes
-    debug: true, // Enable debugging to see cache hits/misses
-    exclude: {
-        query: false // Cache requests with query parameters
-    },
-    key: (req) => {
-        // Custom key generation, e.g., use the full URL + query params
-        return req.url + '?' + new URLSearchParams(req.params).toString();
-    }
-});
-
-const smallerCache = setupCache({
-    maxAge: 1 * 60 * 1000, // 1 minute
-    debug: true,
-    exclude: {
-        query: false
-    }
-});
 
 // Environment variable handling
 const API_KEY = Constants.expoConfig?.extra?.api_key || process.env['API_KEY'];
@@ -30,7 +10,6 @@ console.log(API_URL);
 
 export const apiClient = axios.create({
     baseURL: API_URL,
-    adapter: cache.adapter,
     headers: {
         api_key: API_KEY
     },
@@ -38,7 +17,6 @@ export const apiClient = axios.create({
 
 export const quickApiClient = axios.create({
     baseURL: API_URL,
-    adapter: smallerCache.adapter,
     headers: {
         api_key: API_KEY
     },
