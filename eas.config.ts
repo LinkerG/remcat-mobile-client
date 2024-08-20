acaedc1028493f0382c381b066c9aba7c3bae8cc
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2024-08-20 17:59:20 +0200

    [Build] Prayge

--- eas.config.ts
+++ eas.config.ts
@@ -11,28 +11,16 @@ export default {
             android: {
                 buildType: "apk",
             },
-            env: {
-                API_URL: process.env.API_URL,
-                API_KEY: process.env.API_KEY,
-            },
         },
         preview: {
             distribution: "internal",
             android: {
                 buildType: "apk",
             },
-            env: {
-                API_URL: process.env.API_PROD_URL,
-                API_KEY: process.env.API_KEY,
-            },
         },
         production: {
             android: {
                 buildType: "apk",
-            },
-            env: {
-                API_URL: process.env.API_PROD_URL,
-                API_KEY: process.env.API_KEY,
             },
         }
     },
